8334c507152521c8290fdeb0cf616cbb9da4d0d8
Jack Chang <wei0831@gmail.com>
2016-06-20 21:03:20 -0700

    update script

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-for dir in `find . -name "js-*" -type d -maxdepth 1`
+found_error=0
+
+for dir in `find . -maxdepth 1 -name "js-*" -type d `
 do
   if [ -d "${dir}" ]; then
       cd "${dir}"
-      npm install -s 
-      grunt check
-      npm test
+      npm install -s
+
+      grunt check &> grunt-output.log
+      if cat grunt-output.log | grep -q 'Aborted'; then
+        found_error=1
+      fi
+      cat grunt-output.log
+      rm grunt-output.log
+
+      npm test &> npmtest-output.log
+      if cat npmtest-output.log | grep -q 'ERR!'; then
+        found_error=1
+      fi
+      cat npmtest-output.log
+      rm npmtest-output.log
+
       cd ..
   fi
 done
+
+exit ${found_error}
