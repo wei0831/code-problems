3e53dd665f5f7761e825410d73a422d8e545d487
Jack Chang <wei0831@gmail.com>
2016-06-20 21:18:21 -0700

    update ci script

--- .travis-test.sh
+++ .travis-test.sh
@@ -6,22 +6,28 @@ do
   if [ -d "${dir}" ]; then
       cd ${dir}
 
-      echo "Testing...[" ${dir} "]"
+      echo "[Testing]...[" ${dir} "]...[Start]"
+
+      echo "[npm install]..."
       npm install -s > /dev/null
 
+      echo "[grunt check]..."
       grunt check &> grunt-output.log
       if cat grunt-output.log | grep -q 'Aborted'; then
+        cat grunt-output.log
         found_error=1
       fi
-      cat grunt-output.log
       rm grunt-output.log
 
+      echo "[npm test]..."
       npm test &> npmtest-output.log
       if cat npmtest-output.log | grep -q 'ERR!'; then
+        cat npmtest-output.log
         found_error=1
       fi
-      cat npmtest-output.log
       rm npmtest-output.log
+
+      echo "[Testing]...[" ${dir} "]...[End]"
 
       cd ..
   fi
