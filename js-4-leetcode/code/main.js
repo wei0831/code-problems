e0ae731a4c9ddbcd0150dbe02833e4e350d58c0e
Jack Chang <wei0831@gmail.com>
2016-06-27 04:02:28 -0700

    js-4-leetcode start

---
+++ js-4-leetcode/code/main.js
@@ -0,0 +1,34 @@
+/**
+ * Authoer: Jack Chang
+ * Data: 06/20/2016
+ */
+
+// Implemention
+function Implemention() {
+    "use strict";
+
+    var private_function = function() {
+
+        return true;
+    };
+
+    return {
+        result: function() {
+            return private_function();
+        }
+    };
+}
+
+// Export Functions
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+    module.exports = Implemention;
+}
+
+// Main
+(function() {
+    "use strict";
+
+    // var answer = new Implemention();
+    // console.log("This is main : ", answer.result());
+
+})();
