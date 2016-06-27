fb0472be2f4fdaad36d0dd791953a9185a23975f
Jack Chang <wei0831@gmail.com>
2016-06-27 06:13:55 -0700

    js-4-leetcode finish

--- js-4-leetcode/code/main.js
+++ js-4-leetcode/code/main.js
@@ -7,14 +7,67 @@
 function Implemention() {
     "use strict";
 
-    var private_function = function() {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    var findMedianSortedArrays = function(nums1, nums2) {
+        var len_total = nums1.length + nums2.length;
+        if (len_total === 0) {
+            return 0;
+        }
 
-        return true;
+        if (len_total & 0x1) {
+            return findKthSmallest(nums1, 0, nums2, 0, Math.ceil(len_total / 2));
+        } else {
+
+            return (findKthSmallest(nums1, 0, nums2, 0, (len_total / 2)) +
+                findKthSmallest(nums1, 0, nums2, 0, (len_total / 2 + 1))) / 2;
+        }
     };
 
+    /**
+     * @param {number[]} ar1
+     * @param {number} start1
+     * @param {number[]} ar2
+     * @param {number} start2
+     * @param {number} k
+     * @return {number}
+     */
+    var findKthSmallest = function(ar1, start1, ar2, start2, k) {
+        var len1 = ar1.length - start1;
+        var len2 = ar2.length - start2;
+
+        if (len1 > len2) {
+            return findKthSmallest(ar2, start2, ar1, start1, k);
+        }
+
+        if (len1 === 0) {
+            return ar2[start2 + k - 1];
+        }
+
+        if (k === 1) {
+            return Math.min(ar1[start1], ar2[start2]);
+        }
+
+        var mid1 = Math.min(len1, k >> 1);
+        var mid2 = k - mid1;
+        var num1 = ar1[start1 + mid1 - 1];
+        var num2 = ar2[start2 + mid2 - 1];
+
+        if (num1 < num2) {
+            return findKthSmallest(ar1, start1 + mid1, ar2, start2, k - mid1);
+        } else if (num1 > num2) {
+            return findKthSmallest(ar1, start1, ar2, start2 + mid2, k - mid2);
+        } else {
+            return num1;
+        }
+    }
+
     return {
-        result: function() {
-            return private_function();
+        result: function(nums1, nums2) {
+            return findMedianSortedArrays(nums1, nums2);
         }
     };
 }
@@ -29,6 +82,6 @@ if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
     "use strict";
 
     // var answer = new Implemention();
-    // console.log("This is main : ", answer.result());
+    // console.log(answer.result([1, 2], [3, 4, 5]));
 
 })();
--- js-4-leetcode/spec/specMain.js
+++ js-4-leetcode/spec/specMain.js
@@ -13,11 +13,168 @@ describe("Test Code Problem", function() {
     });
 });
 
-describe("Some Test", function() {
+describe("Two Sorted Array Test Cases", function() {
     "use strict";
 
-    it("expects true to be true", function() {
-        expect(answer.result()).toEqual(true);
+    it("Input [],[] = 0", function() {
+        var input1 = [];
+        var input2 = [];
+        var output = 0;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3,4,5,6,7,8,9],[] = 5", function() {
+        var input1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        var input2 = [];
+        var output = 5;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [],[1,2,3,4,5,6,7,8,9] = 5", function() {
+        var input1 = [];
+        var input2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        var output = 5;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1],[] = 1", function() {
+        var input1 = [1];
+        var input2 = [];
+        var output = 1;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1],[1] = 1", function() {
+        var input1 = [1];
+        var input2 = [1];
+        var output = 1;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1],[1,2] = 1", function() {
+        var input1 = [1];
+        var input2 = [1, 2];
+        var output = 1;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [3],[1,2] = 2", function() {
+        var input1 = [3];
+        var input2 = [1, 2];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3],[4,5,6] = 3.5", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [4, 5, 6];
+        var output = 3.5;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,3,5],[2,4,6] = 3.5", function() {
+        var input1 = [1, 3, 5];
+        var input2 = [2, 4, 6];
+        var output = 3.5;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [3,5,7],[1,2,3,4] = 3", function() {
+        var input1 = [3, 5, 7];
+        var input2 = [1, 2, 3, 4];
+        var output = 3;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [2,2,2],[2,2,2] = 2", function() {
+        var input1 = [2, 2, 2];
+        var input2 = [2, 2, 2];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [2,2,2],[2,2,2,2] = 2", function() {
+        var input1 = [2, 2, 2];
+        var input2 = [2, 2, 2, 2];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3],[1,2,3] = 2", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [1, 2, 3];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3],[1,2,3,4] = 2", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [1, 2, 3, 4];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3],[1,2,3,4] = 2", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [1, 2, 3, 4];
+        var output = 2;
+
+        expect(answer.result(input1, input2)).toEqual(output);
+    });
+
+});
+
+describe("Two Random Sorted Array Test Cases", function() {
+    var input1 = [];
+    var input2 = [];
+    var output;
+    var i;
+    var getRandomIntInclusive = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    var shuffle = function(array) {
+        var currentIndex = array.length;
+        var temporaryValue;
+        var randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    };
+
+    for (i = 0; i < getRandomIntInclusive(10, 30); ++i) {
+        input1.push(getRandomIntInclusive(10, 30));
+    }
+    input1.sort();
+    var half = Math.floor(input1.length / 2);
+    output = (input1.length % 2) ? input1[half] : (input1[half - 1] + input1[half]) / 2;
+    shuffle(input1);
+    input2 = input1.splice(0, getRandomIntInclusive(0, input1.length));
+    input1.sort();
+    input2.sort();
+    
+    it("Input [" + input1 + "], [" + input2 + "] = " + output, function() {
+        expect(answer.result(input1, input2)).toEqual(output);
     });
 
 });
