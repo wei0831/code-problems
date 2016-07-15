5a29bde69fed52340a8388fa7fd789af4881f251
Jack Chang <wei0831@gmail.com>
2016-07-15 14:33:36 -0700

    js-349-leetcode finish

--- js-349-leetcode/code/main.js
+++ js-349-leetcode/code/main.js
@@ -12,12 +12,31 @@ function Implementation() {
     "use strict";
 
     /**
-     * private Function
+     * intersection of integer array
      *
      * @function
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
      */
     var intersection = function(nums1, nums2) {
+        var hash = {};
+        var result = [];
 
+        for (var i = 0; i < nums1.length; ++i) {
+            hash[nums1[i]] = 1;
+        }
+
+        for (var j = 0; j < nums2.length; ++j) {
+            if (hash[nums2[j]] !== undefined) {
+                ++hash[nums2[j]];
+                if (hash[nums2[j]] === 2) {
+                    result.push(nums2[j]);
+                }
+            }
+        }
+
+        return result;
     };
 
     return {
@@ -31,7 +50,8 @@ function Implementation() {
 // (function() {
 //     "use strict";
 //
-//     // var answer = new Implementation();
+//     var answer = new Implementation();
+//     console.log(answer.v1([1,2,2,1], [2,2]));
 //
 // }());
 
--- js-349-leetcode/spec/specMain.js
+++ js-349-leetcode/spec/specMain.js
@@ -16,4 +16,59 @@ describe("Test Code Problem", function() {
 describe("V1 Test Cases", function() {
     "use strict";
 
+    it("Input [], [] = []", function() {
+        var input1 = [];
+        var input2 = [];
+        var output = [];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3], [] = []", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [];
+        var output = [];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1], [2] = []", function() {
+        var input1 = [1];
+        var input2 = [2];
+        var output = [];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,1,1], [1] = [1]", function() {
+        var input1 = [1, 1, 1];
+        var input2 = [1];
+        var output = [1];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3], [4] = []", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [4];
+        var output = [];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [1,2,3], [2,2] = [2]", function() {
+        var input1 = [1, 2, 3];
+        var input2 = [2, 2];
+        var output = [2];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
+
+    it("Input [4,4], [4,4] = [4]", function() {
+        var input1 = [4, 4];
+        var input2 = [4, 4];
+        var output = [4];
+
+        expect(answer.v1(input1, input2)).toEqual(output);
+    });
 });
