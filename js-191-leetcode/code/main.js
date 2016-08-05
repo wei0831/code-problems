9774d2c13b7b613807814516d4c0d2e8833f120d
Jack Chang <wei0831@gmail.com>
2016-08-04 17:17:29 -0700

    js-191-leetcode finish

--- js-191-leetcode/code/main.js
+++ js-191-leetcode/code/main.js
@@ -20,9 +20,9 @@ function Implementation() {
      */
     var hammingWeight = function(n) {
         var result = 0;
-        while(n !== 0){
-          ++result;
-          n &= n -1;
+        while (n !== 0) {
+            ++result;
+            n &= n - 1;
         }
         return result;
     };
