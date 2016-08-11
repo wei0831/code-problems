769b42da87db14e1ec839d36933050c30ed88986
Jack Chang <wei0831@gmail.com>
2016-08-11 02:26:55 -0700

    knight's tour backtracking finish

---
+++ js-knightTour-backtracking/README.md
@@ -0,0 +1,24 @@
+# Code Problem
+
+##Problem
+####Backtracking — Knight’s Tour
+A knight’s tour is a sequence of moves of a knight on a chess­board such that the knight vis­its every square only once. If the knight ends on a square that is one knight’s move from the begin­ning square (so that it could tour the board again imme­di­ately, fol­low­ing the same path), the tour is closed, oth­er­wise it is open. [http://en.wikipedia.org/wiki/Knight%27s_tour](http://en.wikipedia.org/wiki/Knight%27s_tour)
+
+## Requriements
+- Node
+- Grunt
+```
+npm install -g grunt-cli
+```
+
+## How to Run?
+```
+npm install
+npm test
+```
+
+## Before Commit
+Make sure code passes checker
+```
+grunt check
+```
---
+++ js-knightTour-backtracking/code/main.js
@@ -0,0 +1,148 @@
+/**
+ *
+ * @author Jack Chang
+ */
+
+/**
+ * Implementation class
+ *
+ * @class
+ * @param {number} N
+ */
+function Implementation(N) {
+    "use strict";
+    var board;
+    var size = N || 8;
+    var totalSpace = size * size;
+
+    /**
+     * Init - make board
+     *
+     * @function
+     */
+    var _init = function() {
+        board = new Array(size);
+        for (var i = 0; i < board.length; ++i) {
+            board[i] = new Array(size);
+            board[i].fill(0);
+        }
+    };
+
+    /**
+     * Print board
+     *
+     * @function
+     */
+    var printBoard = function() {
+        for (var j = 0; j < size; ++j) {
+            var s = "";
+            for (var i = 0; i < size; ++i) {
+                if (board[i][j] < 10) {
+                    s += "0";
+                }
+                s += board[i][j] + " ";
+
+            }
+            console.log(s);
+        }
+        console.log();
+    };
+
+    /**
+     * private Function
+     *
+     * @function
+     * @param {number} x
+     * @param {number} y
+     * @return {boolean}
+     */
+    var canMove = function(x, y) {
+        return x >= 0 && x < size && y >= 0 && y < size && board[x][y] === 0;
+    };
+
+    /**
+     * private Function
+     *
+     * @function
+     * @return {boolean}
+     */
+    var solve = function() {
+        return tour(0, 0, 1);
+    };
+
+    /**
+     * Recursive Checking Path
+     *
+     * @function
+     * @param {number} x
+     * @param {number} y
+     * @param {number} step
+     * @return {boolean}
+     */
+    var tour = function(x, y, step) {
+        // Found Answer
+        if (step === totalSpace) {
+            board[x][y] = step;
+            return true;
+        }
+
+        board[x][y] = step;
+
+        // Top Right Top
+        if (canMove(x - 1, y - 2) && tour(x - 1, y - 2, step + 1)) {
+            return true;
+        }
+        // Top Right Bot
+        if (canMove(x - 2, y - 1) && tour(x - 2, y - 1, step + 1)) {
+            return true;
+        }
+        // Bot Right Top
+        if (canMove(x - 2, y + 1) && tour(x - 2, y + 1, step + 1)) {
+            return true;
+        }
+        // Bot Right Bot
+        if (canMove(x - 1, y + 2) && tour(x - 1, y + 2, step + 1)) {
+            return true;
+        }
+        // Top Left Top
+        if (canMove(x + 1, y + 2) && tour(x + 1, y + 2, step + 1)) {
+            return true;
+        }
+        // Top Left Bot
+        if (canMove(x + 2, y + 1) && tour(x + 2, y + 1, step + 1)) {
+            return true;
+        }
+        // Bot Left Top
+        if (canMove(x + 2, y - 1) && tour(x + 2, y - 1, step + 1)) {
+            return true;
+        }
+        // Bot Left Bot
+        if (canMove(x + 1, y - 2) && tour(x + 1, y - 2, step + 1)) {
+            return true;
+        }
+
+        board[x][y] = 0;
+
+        return false;
+    };
+
+    _init();
+    return {
+        v1: solve,
+        print: printBoard
+    };
+}
+
+/**
+ * Main
+ */
+(function() {
+    "use strict";
+
+    // CAUTION: It takes a while to get answer....
+    var answer = new Implementation(8);
+    answer.v1();
+    answer.print();
+}());
+
+module.exports = Implementation;
